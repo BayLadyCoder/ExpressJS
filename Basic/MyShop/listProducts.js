3dcc769e4f56667bef253dd8c9d29538f0d1824c
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-19 23:21:57 -0400

    Using npm install and using faker.js and testing it with Node.js

---
+++ Basic/MyShop/listProducts.js
@@ -0,0 +1,22 @@
+/*
+Use 'faker' to print out 10 random product names and prices
+*/
+
+/*
+Terminal
+cd desktop
+mkdir myShop
+touch listProducts.js
+npm install faker
+*/
+
+var faker = require('faker');
+
+for (var i = 0; i < 10; i++) {
+	console.log(faker.commerce.product() + " " + faker.commerce.price());
+}
+
+/*
+Terminal
+node listProducts.js
+*/
