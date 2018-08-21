9c8629f3a7d97e0e5492e86bcd27dba188bcbb05
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-20 21:16:49 -0400

    added /speak/:animal route to Express Routing Exercise.

--- expressRoutingExercise/app.js
+++ expressRoutingExercise/app.js
@@ -6,6 +6,26 @@ app.get("/", function(req, res) {
 	res.send("Hi there! Welcome to my Express Routing Exercise!");
 });
 
+app.get("/speak/:animal", function(req, res) {
+	var animal = req.params.animal;
+
+	if (animal === "pig") {
+		res.send("The " + animal + " says 'Oink'");
+	} else if (animal === "cow") {
+		res.send("The " + animal + " says 'Moo'");
+	} else if (animal === "dog") {
+		res.send("The " + animal + " says 'Woof Woof!'");
+	} else {
+		res.send("The " + animal + " says 'MEEEEOOOOWWWW'");
+	}
+
+	
+});
+
+app.get("/", function(req, res) {
+	res.send("");
+});
+
 app.get("*", function(req, res) {
 	res.send("Sorry, page is not found... What are you doing with your life?");
 });
